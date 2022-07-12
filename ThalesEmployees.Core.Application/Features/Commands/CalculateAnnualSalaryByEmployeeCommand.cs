0efce1e2a881df0bed97e48c2aa9ba52eb2cb561
sonatedev@outlook.com <sonatedev@outlook.com>
2022-07-12 12:55:54 -0500

    calculate salary commands

---
+++ ThalesEmployees.Core.Application/Features/Commands/CalculateAnnualSalaryByEmployeeCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThalesEmployees.Core.Application.Exceptions;
+using ThalesEmployees.Core.Domain.Contracts;
+
+namespace ThalesEmployees.Core.Application.Features.Commands
+{
+    public record CalculateAnnualSalaryByEmployeeRequest(int EmployeeId) : IRequest<decimal>;
+    public class CalculateAnnualSalaryByEmployeeCommand : IRequestHandler<CalculateAnnualSalaryByEmployeeRequest, decimal>
+    {
+        private readonly IReadRepositoryEmployee _readRepositoryEmployee;
+
+        public CalculateAnnualSalaryByEmployeeCommand(IReadRepositoryEmployee readRepositoryEmployee)
+        {
+            _readRepositoryEmployee = readRepositoryEmployee;
+        }
+
+        public async Task<decimal> Handle(CalculateAnnualSalaryByEmployeeRequest request, CancellationToken cancellationToken)
+        {
+            var employee = await _readRepositoryEmployee.GetByIdAsync(request.EmployeeId);
+            _ = employee ?? throw new NotFoundException("Employee not found");
+
+            return employee.AnualSalary;
+        }
+    }
+}
---
+++ ThalesEmployees.Core.Application/Features/Commands/CalculateEmployeeAnnualSalaryCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThalesEmployees.Core.Application.Features.Commands
+{
+    public record CalculateAnnualSalaryRequest(decimal Salary) : IRequest<decimal>;
+
+    public class CalculateEmployeeAnnualSalaryCommand : IRequestHandler<CalculateAnnualSalaryRequest, decimal>
+    {
+        public Task<decimal> Handle(CalculateAnnualSalaryRequest request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(request.Salary * 12);
+        }
+    }
+}
